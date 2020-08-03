e6c673b18b890319a1f17cab2bc117e505914d93
Saar Taranus <saartaranus@gmail.com>
2020-08-03 00:39:55 -0400

    Adding project files

---
+++ mac_changer.py
@@ -0,0 +1,34 @@
+import subprocess
+import optparse
+import re
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+    (user_options, arguments) = parser.parse_args()  # arguments will take --interface, mac and option will take the user input
+    if not user_options.interface:
+        parser.error("[-] Please specify an interface")
+    elif not user_options.new_mac:
+        parser.error("[-] Please specify a new mac")
+    return user_options
+
+
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC address for " + interface + " to " + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+
+options = get_arguments()
+# change_mac(options.interface, options.new_mac)
+
+ifconfig_result = subprocess.check_output(["ifconfig", options.interface])
+print(ifconfig_result)
+
+mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+if mac_address_search_result:
+    print(mac_address_search_result.group(0)) #printing the first result
+else:
+    print("[-] Could not read MAC address")
